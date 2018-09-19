4f2fef6b533d8f6b4defb042ce821f6a8f210a5d
Rodrigo235 <r.rodrigomoliveira@gmail.com>
2018-09-19 16:51:23 -0300

    branch para teeentar add o mapa ao corona

--- Jogo/main.lua
+++ Jogo/main.lua
@@ -12,9 +12,9 @@
 --local director = require ("Sources.diretorio")
 --director:changeScene("Sources.menu")
 
-local tiled = require "com.ponywolf.ponytiled"
-local mapData = require "Objects.mapa.cenario1.cenarioLua"
-local mapa = tiled.new(mapData, "Objects.mapa.cenario1.imagens")
+--local tiled = require "com.ponywolf.ponytiled"
+--local mapData = require "Objects.mapa.cenario1.cenarioLua"
+--local mapa = tiled.new(mapData, "Objects.mapa.cenario1.imagens")
 
 local cenarioPorTrasDeTudo = display.newImageRect("imagens/cenario.png", 1000, 600	)
 --local cenario = require ("Objects.cenario")
