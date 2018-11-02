958dcf416cbdcf0386db165326219c99dbd9c302
Rodrigo235 <r.rodrigomoliveira@gmail.com>
2018-11-02 15:02:12 -0300

    troca de branch

--- Jogo/view/cenario.lua
+++ Jogo/view/cenario.lua
@@ -46,6 +46,7 @@ end
 function cenario:removerEventos(id)
 	if(id == 2) then
 		cenario:removerEvento()
+		print ("chamei remover evento")
 		direcional:removerEvento()
 		botaoBomba:removerEvento()
 	elseif (id == 4) then
