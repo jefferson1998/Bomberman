d31542f96867994fbc8f58da2e6d9252cb6a6dd6
jefferson1998 <jefferson05061998@gmail.com>
2018-11-03 11:55:28 -0300

    tentando

--- Jogo/Objects/buscaEmLargura.lua
+++ Jogo/Objects/buscaEmLargura.lua
@@ -5,54 +5,39 @@ local inimigo = cenario:getInimigoView()
 local posPersonagemX, posPersonagemY = map:pixelToBoard(cenario:getMapa():localizarNoMapa(cenario:getPersonagem():getSprite()))
 
 local posAnteriorX, posAnteriorY
-function buscaEmLargura:isResultadoBusca()
-	local posPersonagemX, posPersonagemY = map:pixelToBoard(cenario:getMapa():localizarNoMapa(cenario:getPersonagem():getSprite()))
-
-	if posPersonagemX == posInimigoX and posPersonagemY == posInimigoY then
-		print("ACHEI O PERSONAGEM")
-		return true
-	end
-	return false
-end
 
 function buscaEmLargura:buscar()
 local posInimigoX, posInimigoY =  map:pixelToBoard(cenario:getMapa():localizarNoMapa(cenario:getInimigoView():getSprite()))
 
-	if buscaEmLargura:isResultadoBusca() then
-		-- Para o jogo
-	else
-		-- direita
-		if estadoDoJogo[posInimigoX][posInimigoY + 1] ~= nil and estadoDoJogo[posInimigoX][posInimigoY + 1] == 0 then
+	-- direita
+	if estadoDoJogo[posInimigoX][posInimigoY + 1] ~= nil and estadoDoJogo[posInimigoX][posInimigoY + 1] == 0 then
 
-			if posInimigoX == posPersonagemX and posInimigoY + 1 == posPersonagemY then
-				-- Para o jogo
-				print("ACHEI O PERSONAGEM")
-			end 
-		-- esquerda
-		elseif estadoDoJogo[posInimigoX][posInimigoY - 1] ~= nil and estadoDoJogo[posInimigoX][posInimigoY - 1] == 0 then
-			 
-			if posInimigoX == posPersonagemX and posInimigoY  - 1 == posPersonagemY then
+		if posInimigoX == posPersonagemX and posInimigoY + 1 == posPersonagemY then
 			-- Para o jogo
 			print("ACHEI O PERSONAGEM")
-			end
-		-- cima 
-		elseif estadoDoJogo[posInimigoX - 1][posInimigoY]  ~= nil and  estadoDoJogo[posInimigoX - 1][posInimigoY] ~= 0 then
-			
-			if posInimigoX - 1 == posPersonagemX and posInimigoY == posPersonagemY then
-				-- Para o jogo
-				print("ACHEI O PERSONAGEM")
-			end 
-		-- baixo
-		elseif estadoDoJogo[posInimigoX + 1][posInimigoY]  ~= nil and estadoDoJogo[posInimigoX + 1][posInimigoY] ~= 0 then
-			 
-			if posInimigoX +1 == posPersonagemX and posInimigoY == posPersonagemY then
-				-- Para o jogo
-				print("ACHEI O PERSONAGEM")
-			end 
-
+		end 
+	-- esquerda
+	elseif estadoDoJogo[posInimigoX][posInimigoY - 1] ~= nil and estadoDoJogo[posInimigoX][posInimigoY - 1] == 0 then
+		 
+		if posInimigoX == posPersonagemX and posInimigoY  - 1 == posPersonagemY then
+		-- Para o jogo
+		print("ACHEI O PERSONAGEM")
 		end
-		-- print("P. ANT X___ " .. posAnteriorX)
-		-- print("P. ANT Y___ " .. posAnteriorY)
+	-- cima 
+	elseif estadoDoJogo[posInimigoX - 1][posInimigoY]  ~= nil and  estadoDoJogo[posInimigoX - 1][posInimigoY] ~= 0 then
+		
+		if posInimigoX - 1 == posPersonagemX and posInimigoY == posPersonagemY then
+			-- Para o jogo
+			print("ACHEI O PERSONAGEM")
+		end 
+	-- baixo
+	elseif estadoDoJogo[posInimigoX + 1][posInimigoY]  ~= nil and estadoDoJogo[posInimigoX + 1][posInimigoY] ~= 0 then
+		 
+		if posInimigoX +1 == posPersonagemX and posInimigoY == posPersonagemY then
+			-- Para o jogo
+			print("ACHEI O PERSONAGEM")
+		end 
+
 	end
 end
 function buscaEmLargura:atualizarInimigo()
@@ -106,7 +91,6 @@ end
 
 function buscaEmLargura:enterFrame()
 	inimigo:enterFrame()
-	buscaEmLargura:isResultadoBusca()
 	buscaEmLargura:buscar()
 	buscaEmLargura:atualizarInimigo()
 end
