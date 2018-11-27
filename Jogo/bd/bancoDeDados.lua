fb7086212278fdfac91cfad6b914754220232f87
AsaphSouza <asaphsouza1605@gmail.com>
2018-11-26 23:13:14 -0200

    atualizações no banco de dados

--- Jogo/bd/bancoDeDados.lua
+++ Jogo/bd/bancoDeDados.lua
@@ -1,26 +1,29 @@
 local sqlite3 = require("sqlite3")
 
 path = system.pathForFile("banco.db", system.DocumentsDirectory)
+local db = sqlite3.open(path)
 
-function openAccessBD (path)
-    return sqlite3.open(path)
-end 
-
-local db = openAccessBD(path)
-
-local criarTabelaJogador = [[CREATE TABLE IF NOT EXISTS jogador ( idJogador INTEGER PRIMARY KEY autoincrement, nome, pontuacao INTEGER );]]
+local criarTabelaJogador = [[CREATE TABLE IF NOT EXISTS jogador ( idJogador INTEGER PRIMARY KEY autoincrement, nome, vitorias INTEGER );]]
 db:exec(criarTabelaJogador)
 
---TEM QUE CRIAR UMA FUNÇÃO QUE RECEBE UM JOGADOR E O INSERE.
+function inserir_personagem (personagem)
+    local inserir = [[INSERT INTO jogador VALUES ( NULL,"]] .. personagem.nome .. [[","]] .. personagem.pontuacao .. [[" );]]
+    db:exec(inserir)
+end 
 
---local player = jogador("Pereira", 550)
+function deletar_personagem (personagem)
+    local deletar = [[DELETE * FROM jogador WHERE nome = "]] .. personagem.nome .. [[";]]
+    db:exec(deletar)
+end
 
-local q = [[INSERT INTO test VALUES ( NULL, "]] .. player.nome .. [[","]] .. player.pontuacao .. [[" );]]
-db:exec( q )
+function alterar_nome_personagem (nomeAtual, nomePretendido)
+    local alterarNome = [[UPDATE JOGADOR SET nome = "]] .. nomePretendido .. [[" WHERE nome IN ("]] .. nomeAtual .. [[");]]
+    db:exec(alterarNome)
+end
 
 function recuperarJogadores ()
     local jogadores = {}
-    for row in db:nrows( "SELECT * FROM jogador" ) do
+    for row in db:nrows( "SELECT * FROM jogador ORDER BY " ) do
         jogadores[#jogadores+1] =
         {
             nome = row.nome,
