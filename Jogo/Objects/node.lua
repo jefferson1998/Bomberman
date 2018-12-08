059d40afe3a04090430458a28efda5a315f0a701
Rodrigo235 <r.rodrigomoliveira@gmail.com>
2018-12-08 14:29:47 -0300

    algumas imageens de Icone

--- Jogo/Objects/node.lua
+++ Jogo/Objects/node.lua
@@ -5,20 +5,71 @@ function no:calcularDistancia(px, py)
 	return math.abs(px - personagemPosX) + math.abs(py - personagemPosY)
 end
 
+function no:verificarDistanciaBomba(px, py, mapa)
+
+	local cima, baixo, direita, esquerda = true, true, true, true
+
+	for i = 1, 4 do
+		if(baixo == true and mapa[px + i][py] ~= 0 and mapa[px + i][py] ~= nil) then
+			if(mapa[px + i][py] == 5)then
+				return i
+			end
+		else
+			baixo = false
+		end
+		
+		if(cima == true and mapa[px - i][py] ~= 0 and mapa[px - i][py] ~= nil) then
+			if(mapa[px - i][py] == 5)then
+				return i
+			end
+		else 
+			cima = false
+		end
+		
+		if(direita == true and mapa[px][py + i] ~= 0 and mapa[px][py + i] ~= nil) then
+			if(mapa[px][py + i] == 5)then
+				return i
+			end
+		else
+			direita = false
+		end
+		
+		if(esquerda == true and mapa[px][py - i] ~= 0 and mapa[px][py - i] ~= nil) then
+			if(mapa[px][py - i] == 5)then
+				return i
+			end
+		else
+			esquerda = false
+		end
+	end
+
+	return 0
+
+end
+
 function no:validarVizinho(px, py, direcao)
 	local estado = cenario:getEstadoJogo():getEstado()
+	local distanciaAtualDaBomba = self:verificarDistanciaBomba(px, py, estado)
 
 	if(direcao == "cima" and estado[px - 1][py] ~= 0 and estado[px - 1][py] ~= 3 and estado[px - 1][py] ~= 5 and estado[px - 1][py] ~= nil) then
-		return true
+		if(distanciaAtualDaBomba == 0 or distanciaAtualDaBomba > self:verificarDistanciaBomba(px - 1, py, estado))then
+			return true
+		end
 	end
 	if(direcao == "direita" and estado[px][py + 1] ~= 0 and estado[px][py + 1] ~= 3 and estado[px][py + 1] ~= 5 and estado[px][py + 1] ~= nil) then
-		return true
+		if(distanciaAtualDaBomba == 0 or distanciaAtualDaBomba > self:verificarDistanciaBomba(px, py + 1, estado))then
+			return true
+		end
 	end
 	if(direcao == "baixo" and estado[px + 1][py] ~= 0 and estado[px + 1][py] ~= 3 and estado[px + 1][py] ~= 5 and estado[px + 1][py] ~= nil) then
-		return true
+		if(distanciaAtualDaBomba == 0 or distanciaAtualDaBomba > self:verificarDistanciaBomba(px + 1, py, estado))then
+			return true
+		end
 	end
 	if(direcao == "esquerda" and estado[px][py - 1] ~= 0 and estado[px][py - 1] ~= 3 and estado[px][py - 1] ~= 5 and estado[px][py - 1] ~= nil) then
-		return true
+		if(distanciaAtualDaBomba == 0 or distanciaAtualDaBomba > self:verificarDistanciaBomba(px, py - 1, estado))then
+			return true
+		end
 	end
 end
 
@@ -27,27 +78,15 @@ function no:gerarVizinhos(no)
 	
 	if(self:validarVizinho(no.px, no.py, "cima") == true) then
 		table.insert(listaVizinhos, self:new(no.px - 1, no.py, no, no.G + 1))
-		-- print("ENTREI CIMA")
-		-- print("Y __" .. no.py)
-		-- print("X __" .. no.px)
 	end
 	if(self:validarVizinho(no.px, no.py, "direita") == true) then
 		table.insert(listaVizinhos, self:new(no.px, no.py + 1, no, no.G + 1))
-		-- -- print("ENTREI DIREITA")
-		-- -- print("X __" .. no.px)
-		-- -- print("Y __" .. no.py)
 	end
 	if(self:validarVizinho(no.px, no.py, "baixo") == true) then
 		table.insert(listaVizinhos, self:new(no.px + 1, no.py, no, no.G + 1))
-		-- -- print("ENTREI BAIXO")
-		-- -- print("X __" .. no.px)
-		-- -- print("Y __" .. no.py)
 	end
 	if(self:validarVizinho(no.px, no.py, "esquerda") == true) then
 		table.insert(listaVizinhos, self:new(no.px, no.py - 1, no, no.G + 1))
-		-- -- print("ENTREI ESQUERDA")
-		-- -- print("X __" .. no.px)
-		-- -- print("Y __" .. no.py)
 	end
 
 	return listaVizinhos
